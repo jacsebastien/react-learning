292b26af5d47e7361a9f6cf8ef0e26406a237e84
Sébastien Jacques <sja@bizzdev.net>
2019-11-19 17:17:21 +0100

    Use CSS module on Person

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,30 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
 
-// import './Person.css';
-
-// Create a component that is a div with style thx to styled-components
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-
-    @media screen and (min-width: 500px) {
-            width: 450px;
-    }
-`;
+import styles from './Person.module.css';
 
 const person = props => {
     return (
-        <StyledDiv>
+        <div className={styles.person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old !</p>
             <p>{props.children}</p>
             {/* 2 way binding */}
             <input type="text" onChange={props.changed} value={props.name} />
-        </StyledDiv>
+        </div>
     );
 }
 
