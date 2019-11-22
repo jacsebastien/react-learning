0f7e9b6bbf4b5558e02189883b34bad6b21e953f
Sébastien Jacques <sja@bizzdev.net>
2019-11-22 14:49:35 +0100

    Removing tests

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,11 +3,6 @@ import React from 'react';
 import styles from './Person.module.css';
 
 const person = props => {
-
-    if(Math.random() > 0.7) {
-        throw new Error('Something went wrong');
-    }
-
     return (
         <div className={styles.person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old !</p>
