2c9555a2fab76fb50b2a7b9c3610b5e3de2c5bd7
Sébastien Jacques <sja@bizzdev.net>
2019-11-26 10:32:23 +0100

    Use React.Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
-import Auxiliary from '../../../hoc/Auxiliary';
+import Aux from '../../../hoc/Auxiliary';
 import styles from './Person.module.css';
 
 class Person extends Component {
@@ -14,14 +14,24 @@ class Person extends Component {
         //         <input key="i3" type="text" onChange={this.props.changed} value={this.props.name} />
         // ];
 
-        // Use empty wrapper to avoid using extra JSX element
+        // // Use empty wrapper to avoid using extra JSX element
+        // return (
+        //     <Auxiliary>
+        //         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old !</p>
+        //         <p>{this.props.children}</p>
+        //         {/* 2 way binding */}
+        //         <input type="text" onChange={this.props.changed} value={this.props.name} />
+        //     </Auxiliary>
+        // );
+
+        // Use React Fragment as an equivalent of custom Aux wrapper
         return (
-            <Auxiliary>
+            <Fragment>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old !</p>
                 <p>{this.props.children}</p>
                 {/* 2 way binding */}
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </Auxiliary>
+            </Fragment>
         );
 
         // Wrap multiple JSX elements into one main element
--- src/hoc/Auxiliary.js
+++ src/hoc/Auxiliary.js
@@ -1,3 +1,3 @@
-const Auxiliary = props => props.children;
+const Aux = props => props.children;
 
-export default Auxiliary;
+export default Aux;
