3df040afb3d350cb5c33635366a7cf1183d32841
Sébastien Jacques <sja@bizzdev.net>
2019-12-24 10:06:50 +0100

    Using interceptors

--- 02-http/src/index.js
+++ 02-http/src/index.js
@@ -3,6 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Axios from 'axios';
+
+Axios.interceptors.request.use(config => {
+    console.log(config);
+
+    // ... Edit config ...
+
+    // Always return the confing to avoid blocking requests
+    return config;
+}, error => {
+    // Only catch request errors but not response error
+    console.log(error);
+
+    // Always return error to forward it to component that makes request
+    return Promise.reject(error);
+});
+
+Axios.interceptors.response.use(config => {
+    console.log(config);
+    return config;
+}, error => {
+    // Only catch response errors
+    console.log(error);
+    return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
