1c95bd1796be18b3c931c858f4ef0fdb719dbb24
Sébastien Jacques <sja@bizzdev.net>
2020-01-24 15:30:35 +0100

    useRef and cleaning up useEffect

--- 05-hooks/src/components/Ingredients/Search.js
+++ 05-hooks/src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,21 +6,31 @@ import './Search.css';
 const Search = React.memo(props => {
     const { afterIngredientsLoaded } = props;   // Object destructuring => convert object properties to normal properties
     const [filterState, setFilterState] = useState('');
+    const inputRef = useRef();
 
     useEffect(() => {
-        const query = filterState.length === 0 ? '' : `?orderBy="title"&equalTo="${filterState.trim()}"`;
+        const timer = setTimeout(() => {
+            if (filterState === inputRef.current.value) {
+                const query = filterState.length === 0 ? '' : `?orderBy="title"&equalTo="${filterState}"`;
 
-        fetch('https://react-hooks-8b80e.firebaseio.com/ingredients.json' + query)
-            .then(response => response.json())
-            .then(body => {
-                const loadedIngredients = Object.keys(body).map(key => ({
-                    id: key,
-                    title: body[key].title,
-                    amount: body[key].amount
-                }));
+                fetch('https://react-hooks-8b80e.firebaseio.com/ingredients.json' + query)
+                    .then(response => response.json())
+                    .then(body => {
+                        const loadedIngredients = Object.keys(body).map(key => ({
+                            id: key,
+                            title: body[key].title,
+                            amount: body[key].amount
+                        }));
 
-                afterIngredientsLoaded(loadedIngredients);
-            });
+                        afterIngredientsLoaded(loadedIngredients);
+                    });
+            }
+        }, 500);
+
+        // clean up function on re render or unmount for single call
+        return () => {
+            clearTimeout(timer);
+        };
     }, [filterState, afterIngredientsLoaded]);
 
     return (
@@ -29,6 +39,7 @@ const Search = React.memo(props => {
                 <div className="search-input">
                     <label>Filter by Title</label>
                     <input
+                        ref={inputRef}
                         type="text"
                         value={filterState}
                         onChange={event => setFilterState(event.target.value)}
