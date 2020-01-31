9155244068575c5ba9cf46861792dc2497bc118b
Sébastien Jacques <sja@bizzdev.net>
2020-01-31 15:04:15 +0100

    Dispatching actions

--- 06-redux/redux-basics.js
+++ 06-redux/redux-basics.js
@@ -8,6 +8,18 @@ const initialState = {
 // Reducer
 // If state is undefined, init it with initialState
 const rootReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case 'INC_COUNTER':
+            return {
+                ...state,
+                counter: state.counter + 1
+            };
+        case 'ADD_COUNTER':
+            return {
+                ...state,
+                counter: state.counter + action.value
+            };
+    }
     return state;
 };
 
@@ -16,5 +28,8 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 // Dispatching action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState());
 
 // Subscription
