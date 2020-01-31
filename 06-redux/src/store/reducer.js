943d5a6cf7b633c9b227e76e1199493e0398714e
Sébastien Jacques <sja@bizzdev.net>
2020-01-31 16:15:15 +0100

    Dispatching actions from component

--- 06-redux/src/store/reducer.js
+++ 06-redux/src/store/reducer.js
@@ -1,8 +1,30 @@
 const initialState = {
-    counter: 4
+    counter: 0
 };
 
 const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case 'INC':
+            return {
+                ...state,
+                counter: state.counter + 1
+            };
+        case 'DEC':
+            return {
+                ...state,
+                counter: state.counter - 1
+            };
+        case 'ADD':
+            return {
+                ...state,
+                counter: state.counter + 5
+            };
+        case 'SUB':
+            return {
+                ...state,
+                counter: state.counter - 5
+            };
+    }
     return state;
 };
 
