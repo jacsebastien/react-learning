be4a07a3d5c74f047ea7fe85b764089a4d448fb4
Sébastien Jacques <sja@bizzdev.net>
2020-02-06 11:44:01 +0100

    Handling async code in actions

--- 06-redux/src/index.js
+++ 06-redux/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './App';
@@ -29,7 +30,7 @@ const loggerMiddleware = store => {
 // Config to use Redux devtool browsers extensions and connect app to it
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancer(applyMiddleware(loggerMiddleware)));
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(loggerMiddleware, thunk)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
--- 06-redux/src/store/actions/actions.js
+++ 06-redux/src/store/actions/actions.js
@@ -5,7 +5,7 @@ export const SUB = 'SUB';
 export const STORE = 'STORE';
 export const DELETE = 'DELETE';
 
-// Declaring action creator to use it in components
+// Declaring action creators to use it in components
 export const increment = () => {
     return { type: INC };
 };
@@ -18,9 +18,19 @@ export const add = (value) => {
 export const subtract = (value) => {
     return { type: SUB, value: value };
 };
-export const storeResult = (value) => {
+
+export const saveResult = (value) => {
     return { type: STORE, result: value };
 };
+// Action creators can run async code thx to redux-thunk package
+export const storeResult = (value) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(saveResult(value));
+        }, 2000);
+    };
+};
+
 export const deleteResult = (value) => {
     return { type: DELETE, index: value };
 };
