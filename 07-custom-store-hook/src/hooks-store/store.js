2048d4072698a77356fba99d5cb35fac05933341
jacsebastien <jac.sebastien@gmail.com>
2020-02-18 11:57:44 +0100

    Finishing store hook

--- 07-custom-store-hook/src/hooks-store/store.js
+++ 07-custom-store-hook/src/hooks-store/store.js
@@ -6,18 +6,44 @@ let listeners = [];
 let actions = {};
 
 // If properties were put into the custom hook, data will be exclusive to each component that call the hook and not global to the application
-const useStore = () => {
+export const useStore = () => {
     // const [state, setState] = useState(globalState);
     const setState = useState(globalState)[1];
 
-    useEffect(() => {
-        // Each time a component use useStore, it will have his own setState, pushed into listenes array (one setState per component)
-        listeners.push(setState);
+    const dispatch = (actionIdentifier) => {
+        // actions are functions and we pass globalState to it to get a new state
+        const newState = actions[actionIdentifier](globalState);
 
-        return () => {
-            // Remove setState from listeners when component that use useStore is removed
-            listeners = listeners.filter(li => li != setState);
-        };
-    // once setState is used in a component it never changes, so it will not trigger useEffect again and again
-    }, [setState]);
+        // merge newState with old one
+        globalState = { ...globalState, ...newState };
+
+        // Inform all components listening to this store that globaState has changed
+        for (const listener of listeners) {
+            listener(globalState);
+        }
+    };
+
+    useEffect(
+        () => {
+            // Each time a component use useStore, it will have his own setState, pushed into listenes array (one setState per component)
+            listeners.push(setState);
+
+            return () => {
+                // Remove setState from listeners when component that use useStore is removed
+                listeners = listeners.filter((li) => li != setState);
+            };
+            // once setState is used in a component it never changes, so it will not trigger useEffect again and again
+        },
+        [ setState ]
+    );
+
+    return [globalState, dispatch];
+};
+
+export const initStore = (userActions, initialState) => {
+    if(initialState) {
+        globalState = {...globalState, ...initialState};
+    }
+
+    actions = {...actions, ...userActions};
 };
